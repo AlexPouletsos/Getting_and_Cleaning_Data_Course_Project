e023603178c63914b09354688d322c51dbf64da9
AlexPouletsos <30276007+AlexPouletsos@users.noreply.github.com>
2017-09-30 17:55:35 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,9 +8,10 @@ library(plyr)
 library(dplyr) ### Be sure dplyr is loaded after plyr otherwise Summarize function will not work properly.
 
 setwd("./UCI HAR Dataset")
-features <- read.table("features.txt")
-activity_labels <- read.table("activity_labels.txt")
+features <- read.table("features.txt") #Read 'features' file
+activity_labels <- read.table("activity_labels.txt") #Read 'activity_labels' file
 
+###Format Training Data
 setwd("./train") #Change Directory to 'train' folder
 
 x_train <- read.table("X_train.txt") #Read 'X_train' file
@@ -23,6 +24,7 @@ y_train <- read.table("y_train.txt") #Read 'y_train' file
 
 train_table <- cbind(subject_train, y_train2, x_train) #Combine all TRAIN data into one table
 
+###Format Test Data
 setwd("..")
 setwd("./test") #Change Directory to 'test' folder
 
@@ -31,25 +33,30 @@ x_test <- read.table("X_test.txt") #Read 'X_test' file
 subject_test <- read.table("subject_test.txt") #Read 'subject_test' file
         colnames(subject_test) <- "Subject" #add heading to Subject column
 y_test <- read.table("y_test.txt") #Read 'y_test' file
-        y_test2 <- join(y_test, activity_labels, by="V1")[2] #Converts numbers to labels
-        colnames(y_test2) <- "Activity" ###Step 3 (2 of 2) Complete
+        y_test2 <- join(y_test, activity_labels, by="V1")[2] #Converts Activity numbers to labels
+        colnames(y_test2) <- "Activity"
 
 test_table <- cbind(subject_test, y_test2, x_test) #Combine all TEST data into one table
 
-full_table <- rbind(train_table,test_table) ###Step 1 Complete
+###Create Full File
+full_table <- rbind(train_table,test_table)
 
+###Extract only mean and standard deviation
 mean_std <- grep("mean\\(\\)|std\\(\\)", colnames(full_table)) #Stores all columns with the strings "mean()" or "std()"
-full_table_MeanStd <- full_table[c(1,2,mean_std)] ###Step 2 Complete
+full_table_MeanStd <- full_table[c(1,2,mean_std)] 
 
+###Clean up the variable names
 var_names <- gsub("()","", colnames(full_table_MeanStd), fixed = TRUE) #Remove '()' from all columns
 	colnames(full_table_MeanStd) <- var_names
 var_names <- gsub("-","_", colnames(full_table_MeanStd), fixed = TRUE) #Replace all '-' with '_' in all columns
-	colnames(full_table_MeanStd) <- var_names ###Step 4 Complete
+	colnames(full_table_MeanStd) <- var_names
 
+###Tidy data set with average of each variable for each Activity and each Subject.
 TidyData <- full_table_MeanStd %>%
 	group_by(Subject, Activity) %>%
-	summarize_all(funs(mean)) ###Step 5 Complete. Tidy data set with average of each variable for each Activity and each Subject.
+	summarize_all(funs(mean))
 
+###Save text file to working directory
 setwd("..")
 setwd("..")
 write.table(TidyData, "TidyDataSet.txt", row.name=FALSE) #File will be placed in your working directory
